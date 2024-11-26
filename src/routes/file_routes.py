c75e588b945c5c199eca4e3d47f16de8de3acf8c
Paulo Sérgio <paulosergiosrs08@gmail.com>
2024-11-26 16:31:32 -0300

    removed module from domain

--- src/routes/file_routes.py
+++ src/routes/file_routes.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, UploadFile, File, APIRouter
-from domain.Files_processor import Database_manipulation
+
 from domain.datas_processor import DataManipulation
 
 router = APIRouter()
@@ -16,18 +16,3 @@ async def produto_file(file: UploadFile = File(...)):
 @router.post("/importar-vendas/")
 async def venda_file(file: UploadFile = File(...)):
     return await DataManipulation().upload_vendas(file)
-
-
-@router.get("/listar-clientes/")
-async def list_cliente():
-    return Database_manipulation().listing_cliente()
-
-
-@router.get("/listar-produtos/")
-async def list_produto():
-    return Database_manipulation().listing_produtos()
-
-
-@router.get("/listar-vendas/")
-async def list_venda():
-    return Database_manipulation().listing_vendas()
--- src/settings/config.py
+++ src/settings/config.py
@@ -1,4 +1,4 @@
 class Config:
-    URL_Cliente = ''
+    URL_Cliente = 'http://127.0.0.1:5000/cadastro/clientes'
     URL_Produtos = ''
     URL_Vendas = ''
