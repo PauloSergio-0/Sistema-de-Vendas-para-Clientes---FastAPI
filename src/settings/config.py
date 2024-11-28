771eec8fe9da3d255dc591bf947e52ca165bccab
Paulo Sérgio <paulosergiosrs08@gmail.com>
2024-11-27 22:37:46 -0300

    added produto in functios

--- src/settings/config.py
+++ src/settings/config.py
@@ -1,5 +1,5 @@
 class Config:
     URL_Cliente = 'http://127.0.0.1:5000/cadastro/clientes'
-    URL_Produtos = ''
+    URL_Produtos = 'http://127.0.0.1:5000/cadastro/produto'
     URL_Vendas = ''
     URL_AUTHORIZATION = 'http://127.0.0.1:5000/protected'
