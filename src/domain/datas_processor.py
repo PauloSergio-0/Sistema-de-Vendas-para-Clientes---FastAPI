bf3d58d0a29f69e3ac1ed480987abd022ab98af6
Paulo Sérgio <paulosergiosrs08@gmail.com>
2024-12-10 16:58:18 -0300

    addef format code

--- src/domain/datas_processor.py
+++ src/domain/datas_processor.py
@@ -16,15 +16,13 @@ class DataManipulation:
                 data_decode = io.StringIO(data.decode('utf-8'))
                 cliente =  p.read_csv(data_decode, sep=',')
                 
-                
                 colunas_clientes = ['ID', 'Nome', 'Endereço', 'Contato']
-                
-                
+
                 for column in cliente.columns:
                     
                     if column not in colunas_clientes:
                         raise HTTPException(status_code= status.HTTP_400_BAD_REQUEST,detail= f"""Erro: Coluna '{str(column)}' não aceita pelo sistema. Verificar a integridade do arquivo {File.filename}""")
-                
+
                 for index, item in cliente.iterrows():
                     json_Cliente = {
                         "id": item['ID'],
@@ -32,33 +30,30 @@ class DataManipulation:
                         "endereco": item["Endereço"],
                         "contato": item['Contato']
                     }
-                    
+
                     self.api_Flask.send_datas("Cliente", json_Cliente)
-                
+
                 return {"mensagem": f"Upload do Arquivo {File.filename} feita com sucesso!!"}
             except Exception as e:
                 raise HTTPException(status_code= status.HTTP_400_BAD_REQUEST,detail= f"Erro ao maniplular csv: {str(e)} ")
-            
-            #Success 
+
         else:
             raise HTTPException(status_code= status.HTTP_406_NOT_ACCEPTABLE,detail= f"Erro o arquivo deve ser no formato .csv: {str(e)} ")
-        
-        
+
     async def Upload_Produtos(self, File: UploadFile):
         if File.filename.endswith(".csv"):
             try:
                 data = await File.read()
                 data_decode = io.StringIO(data.decode('utf-8'))
                 produto = p.read_csv(data_decode, sep = ',')
-                
-                
+
                 colunas_Produtos = ['ID', 'Nome', 'Código', 'Categoria', 'Preço']
                 for coluna in produto.columns:
                     if coluna not in colunas_Produtos:
                         raise HTTPException(status_code= status.HTTP_400_BAD_REQUEST, detail= f"""Erro: Coluna '{str(coluna)}' não aceita pelo sistema. Verificar a integridade do arquivo {File.filename}""")
-                    
+
                 for index, item in produto.iterrows():
-                    
+
                     json_produto = {
                                     "id": item['ID'],
                                     "nome": item['Nome'],
@@ -71,12 +66,12 @@ class DataManipulation:
                 return {"mensagem": f"upload de {File.filename} feito com sucesso!!"}
             except Exception as e:
                 raise HTTPException(status_code= status.HTTP_400_BAD_REQUEST, detail= f"erro ao manipular csv {str(e)}")
-            
+
         else:
             raise HTTPException(status_code=status.HTTP_406_NOT_ACCEPTABLE, detail=f"O arquivo deve ser formato .'csv")
-        
+
     async def upload_vendas(self, File: UploadFile):
-        
+
         if File.filename.endswith('.csv'):
             try:
                 data = await File.read()
@@ -84,11 +79,11 @@ class DataManipulation:
                 vendas = p.read_csv(data_decode, sep= ',')
                 
                 colunas_vendas = ['ID do Cliente', 'ID do Produto', 'Quantidade', 'Data da Venda']
-                
+
                 for coluna in vendas.columns:
                     if coluna not in colunas_vendas:
                         raise HTTPException(status_code= status.HTTP_400_BAD_REQUEST, detail= f"""Erro: Coluna '{str(coluna)}' não aceita pelo sistema. Verificar a integridade do arquivo {File.filename}""")
-                    
+
                 for index, item in vendas.iterrows():
                     json_venda = {
                         "id_cliente": item['ID do Cliente'],
@@ -97,6 +92,6 @@ class DataManipulation:
                         "data": item['Data da Venda']
                     }
                     self.api_Flask.send_datas('Venda', json_venda)
-                
+
             except Exception as e:
                 raise HTTPException(status_code= status.HTTP_400_BAD_REQUEST, detail= f"erro ao manipular csv {str(e)}") 
